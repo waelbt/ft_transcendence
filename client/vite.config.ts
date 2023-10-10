eb9d2727ff6b02d49a2e61bbadd9799d505c27e9
Wael Boutzougarte <waboutzo@e2r10p11.1337.ma>
2023-10-10 02:30:14 +0100

    TEAM NOTE : fix the mac-session confilct

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,14 +3,14 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
- plugins: [react()],
- server: {
-   host: true,
-   port: 8000, // This is the port which we will use in docker
-   // Thanks @sergiomoura for the window fix
-   // add the next lines if you're using windows and hot reload doesn't work
+  plugins: [react()],
+  server: {
+    // host: true,
+    host: '0.0.0.0',
+    port: 8000,
+    // Use polling for hot reload in environments where fs events don't propagate correctly (e.g., Docker on macOS)
     watch: {
       usePolling: true
     }
- }
+  }
 })
