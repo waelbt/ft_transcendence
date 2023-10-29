67c6877ad3aa0cd0f08d59c98dda9ac2b24066fe
waelbt <boutzougarte@gmail.com>
2023-10-29 01:03:44 +0100

    add ReactQueryDevtools

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,72 +1,37 @@
-// import AllRoutes from './Routes';
-// import { Toaster } from 'react-hot-toast';
-// QueryOptions,
-// QueryClient,
-// QueryClientProvider,
-import { useQuery, useMutation } from '@tanstack/react-query';
-// import { Link } from 'react-router-dom';
+import AllRoutes from './Routes';
+import { Toaster } from 'react-hot-toast';
 
-// const queryClient = new QueryClient({
-//     defaultOptions: {
-//         queries: {
-//             staleTime: Infinity,
-//             cacheTime: Infinity
-//         } as QueryOptions
-//     }
-// });
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import {
+    QueryOptions,
+    QueryClient,
+    QueryClientProvider
+} from '@tanstack/react-query';
 
-type Todo = {
-    id: number;
-    content: string;
-};
-
-let index = 0;
-let todos: Todo[] = [
-    { id: 1, content: 'todo1' },
-    { id: 2, content: 'todo2' }
-];
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            staleTime: Infinity,
+            cacheTime: Infinity
+        } as QueryOptions
+    }
+});
 
 function App() {
-    const { isPending, variables, mutate } = useMutation({
-        mutationFn: async (todo: Todo) => {
-            await new Promise((resolve) => setTimeout(resolve, 1000));
-            todos.push(todo);
-            index++;
-        }
-    });
-    const { data } = useQuery({
-        queryKey: ['todos'],
-        queryFn: () => todos
-    });
-
     return (
-        <>
-            <button onClick={() => mutate({id: index, content: `todo ${index}`})}>add todo </button>
-            <ul>
-                {data
-                    ? data.map((todo) => <li key={todo.id}>{todo.content}</li>)
-                    : null}
-                {isPending && (
-                    <li style={{ opacity: 0.5 }}>{variables.content}</li>
-                )}
-            </ul>
-        </>
+        // ask gpt later abt the provider oders
+        <QueryClientProvider client={queryClient}>
+            <AllRoutes />
+            <Toaster
+                position="top-center"
+                reverseOrder={false}
+                toastOptions={{
+                    duration: 3000
+                }}
+            />
+            <ReactQueryDevtools />
+        </QueryClientProvider>
     );
-
-    // return (
-    //     // ask gpt later abt the provider oders
-    //     <QueryClientProvider client={queryClient}>
-    //         <AllRoutes />
-    //         <Toaster
-    //             position="top-center"
-    //             reverseOrder={false}
-    //             toastOptions={{
-    //                 duration: 3000
-    //             }}
-    //         />
-    //         {/* <ReactQueryDevtools/> */}
-    //     </QueryClientProvider>
-    // );
 }
 
 export default App;
