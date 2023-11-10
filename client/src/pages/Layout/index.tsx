2592dfa71e1c0f22beb69dbc6c0860dbd5bc1092
waelbt <boutzougarte@gmail.com>
2023-11-10 22:26:53 +0100

    backup

--- client/src/pages/Layout/index.tsx
+++ client/src/pages/Layout/index.tsx
@@ -1,5 +1,17 @@
+// import { Outlet } from 'react-router-dom';
 import { ProfileCompletion } from '../ProfileCompletion';
 
 export function Layout() {
-    return <>{false ? <ProfileCompletion /> : <div>Layout</div>}</>;
+    return (
+        <>
+            {false ? (
+                <ProfileCompletion />
+            ) : (
+                <div>
+                    Layout
+                    {/* <Outlet /> */}
+                </div>
+            )}
+        </>
+    );
 }
