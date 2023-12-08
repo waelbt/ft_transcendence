9731c372b5070611d5ff9e8042ec48c6c1752eed
waelbt <boutzougarte@gmail.com>
2023-12-09 00:34:48 +0100

    merge

--- client/src/stores/index.ts
+++ client/src/stores/index.ts
@@ -1,12 +1,45 @@
-import { create } from "zustand";
+import { create } from 'zustand';
 // import { persist, createJSONStorage } from "zustand/middleware";
 // import { api } from "../Api/base";
 
+// {
+//     "user": {
+//       "id": "112965",
+//       "email": "ibouchaf@student.1337.ma",
+//       "HashPassword": null,
+//       "Avatar": null,
+//       "nickName": "ibouchaf",
+//       "fullName": "Issam Bouchafra",
+//       "status": true,
+//       "F2A": false,
+//       "F2A_Secret": null,
+//       "inGame": false,
+//       "createdAt": "2023-12-08T05:08:53.769Z"
+//     },
+//     "friends": [],
+//     "block": []
+//   }
 
+type UserState = {
+    isLogged: boolean;
+    id: string;
+    email: string;
+    avatar: string;
+    nickName: string;
+    fullName: string;
+    createdAt: string;
+    status: boolean;
+    F2A: boolean;
+    inGame: boolean;
+};
 
+// Define a type for the store's actions
+type UserActions = {
+    login: (name: string, avatar: string) => void;
+    logout: () => void;
+};
 
-
-export const useUserStore = create();
+export const useUserStore = create<UserState & UserActions>();
 
 // import { create } from 'zustand';
 
