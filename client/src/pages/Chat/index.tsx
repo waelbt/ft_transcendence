6d67ad5d3f019bedd2d115f2d164c365727a7479
m-bouhaba <maryam.bouhaba@outlook.com>
2023-12-09 01:42:23 +0100

    ch

--- client/src/pages/Chat/index.tsx
+++ client/src/pages/Chat/index.tsx
@@ -1,6 +1,21 @@
-export function Chat() {
+import React, { Component } from "react";
+import "./chatBody.css";
+import ChatList from "../../components/chat/chatList/ChatList";
+
+// import ChatContent from "../chatContent/ChatContent";
+// import UserProfile from "../userProfile/UserProfile";
+
+class ChatBody extends Component {
+  render(): JSX.Element {
     return (
-      <div>Chat</div>
-    )
+      <div className="main__chatbody">
+        <ChatList />
+        {/* <ChatContent />
+        <UserProfile /> */}
+      </div>
+    );
   }
+}
+
+export default ChatBody;
   
