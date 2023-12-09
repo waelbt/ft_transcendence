6525cb32c7fd3d5176bf008060883ffbec35d1c3
waelbt <boutzougarte@gmail.com>
2023-12-09 01:49:14 +0100

    merge

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -19,16 +19,13 @@ import { MulterModule } from '@nestjs/platform-express';
 import { APP_GUARD } from '@nestjs/core';
 import { accessTokenGuard } from './common/guards';
 import { JwtModule } from '@nestjs/jwt';
-<<<<<<< HEAD
 import { JwtStrategy } from './auth/strategy';
 import { ConversationsModule } from './conversations/conversations.module';
 import { ConversationsService } from './conversations/conversations.service';
-=======
 import { Middlware } from './auth/middlware/file.middlware';
 import { UsersController } from './users/controllers/users.controller';
 import { GameModule } from './game/game.module';
 import { ApiCookieAuth } from '@nestjs/swagger';
->>>>>>> main
 
 // @ApiCookieAuth('jwt') // Specify the cookie name, e.g., 'jwt'
 @Module({
@@ -37,14 +34,11 @@ import { ApiCookieAuth } from '@nestjs/swagger';
     AuthModule,
     PrismaOrmModule,
     UsersModule,
-<<<<<<< HEAD
     MulterModule.register({ dest: '/home/sel-ouaf/ft_transcendence/server/uploads' }),
     ConversationsModule,
     // JwtModule.register({secret: process.env.JWT_secret}),
-=======
     // MulterModule.register({ dest: '/home/sel-ouaf/ft_transcendence/server/uploads' }),
     JwtModule.register({secret: process.env.JWT_secret}),
->>>>>>> main
   ],
   providers: [ChatGateway,
     {
--- server/src/auth/strategy/google.strategy.ts
+++ server/src/auth/strategy/google.strategy.ts
@@ -1,12 +1,8 @@
 import { Injectable } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { PassportStrategy } from "@nestjs/passport";
-<<<<<<< HEAD
-import { ExtractJwt, Strategy, VerifiedCallback } from "passport-jwt";
-=======
 import { Strategy } from "passport-google-oauth20";
 
->>>>>>> main
 
 @Injectable()
 export class googleStrategy extends PassportStrategy(
@@ -15,18 +11,9 @@ export class googleStrategy extends PassportStrategy(
     ) {
     constructor (config: ConfigService){
         super({
-<<<<<<< HEAD
-            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-            secretOrKey: config.get('R_JWT_secret'),
-            passReqToCallback: true,
-            clientID: config.get('Google_Client_Id'),
-            ClientSecret: config.get('Google_Secret'),
-            callbackURL: 'http://localhost:4000/google/redirect',
-=======
             clientID: '1010031413538-61qtbs1ba0qjnmld9s5kfe3ciak5m4ho.apps.googleusercontent.com',
             clientSecret: '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',
             callbackURL: 'http://localhost:4000/auth/google/callback',
->>>>>>> main
             scope: ['email' ,'profile'],
         })
     }
