d788729ac923bb02dadd752e83aeb6cbebcef089
soof05 <sel-ouaf.@student.1337.ma>
2023-12-12 04:06:55 -0500

    Awddi mab9itch baghi had contributor

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,3 @@
-// import { Module } from '@nestjs/common';
-// import { ConfigModule } from '@nestjs/config';
-// import { AuthModule } from './auth/auth.module';
-// import { UserModule } from './user/user.module';
-// import { PrismaOrmModule } from './prisma-orm/prisma-orm.module';
-
 // @Module({
 //   imports: [ConfigModule.forRoot({isGlobal: true,}), AuthModule, UserModule, PrismaOrmModule],
 // })
