60a09dc104d88ddf97c72529155d344a048bd73f
simopoza <mannahri@student.1337.ma>
2024-01-15 11:54:26 +0100

    backup

--- server/src/auth/middlware/file.middlware.ts
+++ server/src/auth/middlware/file.middlware.ts
@@ -7,17 +7,14 @@ export class Middlware implements NestMiddleware {
     constructor(private readonly jwt: JwtService) {}
     async use(req: Request, res: Response, next: NextFunction) {
         console.log('zbiiiiii');
-        // const accessToken = req.cookies['accessToken'];
 
         const authHeader = req.headers.authorization;
 
         if (authHeader && authHeader.startsWith('Bearer ')) {
-            var accessToken = authHeader.slice(7); // Remove 'Bearer ' prefix
-            // console.log('access is : ', accessToken);
+            var accessToken = authHeader.slice(7);
         }
 
         if (!accessToken) {
-            console.log('hi');
             return res.status(401).json({ message: 'Unauthorized' });
         }
 
