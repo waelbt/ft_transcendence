2c3b80f82ed22ebb9f2b37caef5a6b244139c395
waelbt <boutzougarte@gmail.com>
2024-01-22 18:31:47 +0100

    backup

---
+++ client/src/hooks/authHook.ts
@@ -0,0 +1,42 @@
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { request } from '../api';
+
+function useAuth() {
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        let isMounted = true; // flag to check if component is mounted
+
+        const checkAuth = async () => {
+            try {
+                const response = await request.get('/auth/checkAuth');
+                if (isMounted) {
+                    setIsAuthenticated(response.data);
+                }
+            } catch (err) {
+                if (isMounted) {
+                    navigate('/login');
+                }
+            }
+        };
+
+        checkAuth();
+
+        // Cleanup function to set isMounted to false
+        return () => {
+            isMounted = false;
+        };
+    }, []);
+
+    // Optional: useEffect to log or perform actions when isAuthenticated changes
+    useEffect(() => {
+        console.log(isAuthenticated);
+        // Perform any actions that depend on updated isAuthenticated here
+    }, [isAuthenticated]);
+
+    return { isAuthenticated };
+}
+
+export default useAuth;
