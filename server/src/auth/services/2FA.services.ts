89c58347ed31013b6f36209fd978010cbe87d6c8
waelbt <boutzougarte@gmail.com>
2024-01-23 00:21:47 +0100

    bc

--- server/src/auth/services/2FA.services.ts
+++ server/src/auth/services/2FA.services.ts
@@ -1,17 +1,21 @@
-import { Injectable, NotFoundException, Res } from "@nestjs/common";
-import { PrismaOrmService } from "src/prisma-orm/prisma-orm.service";
+import { Injectable, NotFoundException, Req, Res } from '@nestjs/common';
+import { PrismaOrmService } from 'src/prisma-orm/prisma-orm.service';
+import { User } from '@prisma/client';
+import * as otplib from 'otplib';
+import * as qrcodeLib from 'qrcode';
 import * as speakeasy from 'speakeasy';
-import { User } from "@prisma/client";
 import * as qrcode from 'qrcode';
 
 @Injectable()
-export class twoFAService{
-    constructor(private prisma: PrismaOrmService){}
-    generate2FASecret(nickName: string){
-        const sercet = speakeasy.generateSecret({name: nickName});
+export class twoFAService {
+    constructor(private prisma: PrismaOrmService) {}
+
+    generate2FASecret(nickName: string) {
+        const sercet = speakeasy.generateSecret({ name: nickName });
         return sercet.base32;
     }
-    async generate2FA(@Res() res, user: User){
+
+    async generate2FA(@Res() res, user: User) {
         const secret = this.generate2FASecret((await user).fullName);
         const otpAuthUrl = speakeasy.otpauthURL({
             secret: secret,
@@ -41,29 +45,47 @@ export class twoFAService{
     //     });
     // }
 
-    async validateTwoFAToken(token, secret) {
-        return speakeasy.totp.verify({
-          secret,
-          encoding: 'base32',
-          token,
-          window: 1, 
-      });
+    // async validateTwoFAToken(token, secret) {
+    //     return (await speakeasy.totp.verify({
+    //       secret,
+    //       encoding: 'base32',
+    //       token,
+    //       window: 1,
+    //   }));
+    // }
+
+    async validateTwoFAToken(token: string, secret: string): Promise<boolean> {
+        const isValidToken = speakeasy.totp.verify({
+            secret,
+            encoding: 'base32',
+            token,
+            window: 1
+        });
+        return isValidToken;
     }
 
-    async validateTwoFA(user: User, code, @Res() res) {
-        const isUser = await this.prisma.user.findUnique({ where: { id: user.id } });
+    async validateTwoFA(@Req() req, code, @Res() res) {
+        console.log(code);
+        const user = await this.prisma.user.findUnique({
+            where: { id: req.user.sub }
+        });
         if (!user) throw new NotFoundException(`User does not exist`);
-    
-        const enteredToken = code.token;
-        const isValidToken = await this.validateTwoFAToken(enteredToken, user.f2A_Secret);
-    
+
+        console.log(user.f2A_Secret);
+        const isValidToken = await this.validateTwoFAToken(
+            code,
+            user.f2A_Secret
+        );
+        console.log('hda : ', isValidToken);
         if (isValidToken) {
-          await this.prisma.user.update({
-            where: { id: user.id },
-            data: { f2A: true },
-          });
-    
-          return res.status(200).json({ message: '2FA validation successful', user });
+            await this.prisma.user.update({
+                where: { id: req.user.sub },
+                data: { f2A: true }
+            });
+
+            return res
+                .status(200)
+                .json({ message: '2FA validation successful', user });
         } else {
             return res.status(401).json({ message: 'Invalid 2FA token' });
         }
